becf2cde87a169a55e2cbad1f7ae2637a92bb1ac
Никита <hukuta47@yandex.ru>
2025-05-25 23:25:58 +0900

    Добавьте файлы проекта.

---
+++ IgoraSoftware/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using IgoraSoftware.Database;
+using System.Linq;
+using System.Windows;
+
+namespace IgoraSoftware
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static public IgoraEntities entities = new IgoraEntities();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            entities.Clients.First();
+        }
+    }
+}
---
+++ IgoraSoftware/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace IgoraSoftware
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(App.entities.Clients.First().Name);
+        }
+    }
+}
